55c439cfe7536dea703d4c1e785f9a9b26fbed7c
kissmatyi <kiss.mate0225@gmail.com>
2021-11-30 14:54:53 +0100

    First part frontend done

--- app/src/main/java/com/example/p_game/Login.java
+++ app/src/main/java/com/example/p_game/Login.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText userName;
+    EditText pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        userName = findViewById(R.id.userName);
+        pw = findViewById(R.id.userPass);
     }
 
     public void toHome(View v){
@@ -21,6 +28,13 @@ public class Login extends AppCompatActivity {
 
     public void toMenu(View v){
         Intent i = new Intent(this, Menu.class);
-        startActivity(i);
+        if(!pw.getText().toString().equals("") && !userName.getText().toString().equals("")){
+            // itt kell majd ellenőrizni hogy a beírt adatok benne vannak-e az adatbázisban
+            startActivity(i);
+        }
+
+        else{
+            Toast.makeText(this, "Please fill the form", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/p_game/Register.java
+++ app/src/main/java/com/example/p_game/Register.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    EditText pw;
+    EditText pwagain;
+    Button regBtn;
+    EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        pw = findViewById(R.id.userPass);
+        pwagain = findViewById(R.id.userPassAgain);
+        userName = findViewById(R.id.userName);
+        regBtn = findViewById(R.id.registerBtn);
     }
 
     public void toHome(View v){
@@ -19,8 +31,19 @@ public class Register extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void toMenu(View v){
-        Intent i = new Intent(this, Menu.class);
-        startActivity(i);
+    public void toMenu(View v) {
+        if(!pw.getText().toString().equals("") && !pwagain.getText().toString().equals("") && !userName.getText().toString().equals("")){
+            if (!(pw.getText().toString().equals(pwagain.getText().toString()))) {
+                Toast.makeText(this, "The 2 passwords must be the same", Toast.LENGTH_LONG).show();
+            } else {
+                Intent i = new Intent(this, Menu.class);
+                // itt kell majd lementeni a belépési adatokat
+                startActivity(i);
+            }
+        }
+
+        else{
+            Toast.makeText(this, "Please fill the form", Toast.LENGTH_SHORT).show();
+        }
     }
 }
