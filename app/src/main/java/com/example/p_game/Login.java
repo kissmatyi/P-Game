bc4a5da89ef97f9bb45c4fbf80bc72b4703337ff
kissmatyi <kiss.mate0225@gmail.com>
2021-12-03 15:44:02 +0100

    Revert "Connected to Firebase, Login and Register done"
    This reverts commit f5510559

--- app/src/main/java/com/example/p_game/Login.java
+++ app/src/main/java/com/example/p_game/Login.java
@@ -1,17 +1,25 @@
 package com.example.p_game;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
     EditText userName;
     EditText pw;
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://p-game-a75c2-default-rtdb.europe-west1.firebasedatabase.app/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +37,29 @@ public class Login extends AppCompatActivity {
     public void toMenu(View v){
         Intent i = new Intent(this, Menu.class);
         if(!pw.getText().toString().equals("") && !userName.getText().toString().equals("")){
-            // itt kell majd ellenőrizni hogy a beírt adatok benne vannak-e az adatbázisban
-            startActivity(i);
+            db.getReference().child("player").addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.child(userName.getText().toString()).exists() &&
+                            dataSnapshot.child(userName.getText().toString()).child("password").getValue().equals(pw.getText().toString())){
+                        startActivity(i);
+                    }
+
+                    else{
+                        Toast.makeText(Login.this, "Please registrate first", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.d("Nem működik", "onCancelled: ");
+                }
+            });
         }
 
         else{
-            Toast.makeText(this, "Please fill the form", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please fill the form", Toast.LENGTH_LONG).show();
         }
     }
 }
