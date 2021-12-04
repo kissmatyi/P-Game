274e9b1b70ef5f49d019dd15755fa92a6bcdbaec
Bertalan Lovas <lovas>
2021-12-04 18:54:57 +0100

    Frontend fix for waiting and game

--- app/src/main/java/com/example/p_game/Waiting.java
+++ app/src/main/java/com/example/p_game/Waiting.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -14,9 +15,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.Random;
 
 public class Waiting extends AppCompatActivity {
-    TextView twGameId;
-    String gameId;
-    FirebaseDatabase db;
+    private String gameId;
+    private FirebaseDatabase db;
+
+    private TextView twGameId;
+    private TextView twPlayer1;
+    private TextView twPlayer2;
+    private TextView twPlayer3;
+    private TextView twPlayer4;
+
+    private ImageView imagePlayer1;
+    private ImageView imagePlayer2;
+    private ImageView imagePlayer3;
+    private ImageView imagePlayer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,19 @@ public class Waiting extends AppCompatActivity {
         this.twGameId = findViewById(R.id.gameID);
         this.twGameId.setText(this.gameId.toUpperCase());
         this.db = new DatabaseConn().getConnection();
+        this.twPlayer1 = findViewById(R.id.twPlayer1);
+        this.twPlayer2 = findViewById(R.id.twPlayer2);
+        this.twPlayer3 = findViewById(R.id.twPlayer3);
+        this.twPlayer4 = findViewById(R.id.twPlayer4);
+        this.imagePlayer1 = findViewById(R.id.imagePlayer1);
+        this.imagePlayer2 = findViewById(R.id.imagePlayer2);
+        this.imagePlayer3 = findViewById(R.id.imagePlayer3);
+        this.imagePlayer4 = findViewById(R.id.imagePlayer4);
+        loadPlayers();
+    }
+
+    private void loadPlayers() {
+
     }
 
     public void toMenu(View v){
